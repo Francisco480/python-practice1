b1c901f4b7dbd043e828ed8ed995199bea7d5e20
Francisco Poli Veliz <polivelizfrancisco@gmail.com>
2024-04-03 12:21:57 -0300

    Subida de los ejercicios terminados de la práctica 1.

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -9,7 +9,7 @@ Restricción: Usar el operador de multiplicación
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = lado_cuadrado * lado_cuadrado
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -22,7 +22,7 @@ Re-Escribir usando el operador de potencia.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = lado_cuadrado**2
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -35,7 +35,8 @@ Re-Escribir usando la función pow.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
-
+area_cuadrado = pow(lado_cuadrado, 2)
+print(area_cuadrado)
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -50,7 +51,8 @@ precio = 3.74
 presupuesto_disponible = 10
 
 # COMPLETAR - INICIO
-
+cantidad_a_comprar = int(presupuesto_disponible // precio)
+print(cantidad_a_comprar)
 # COMPLETAR - FIN
 
 assert cantidad_a_comprar == 2
@@ -64,7 +66,7 @@ Restricción: Usar el operador módulo.
 numero_incalculable = 2 ** 54 - 1
 
 # COMPLETAR - INICIO
-
+es_divisible_por_siete = numero_incalculable %7 == 0
 # COMPLETAR - FIN
-
+print(es_divisible_por_siete)
 assert es_divisible_por_siete
